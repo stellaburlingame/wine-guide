3943281f4d645f4d8f61af646d65815934cf4e96
Daniel Vasquez <main@dvasquez4155.com>
2025-07-06 14:11:27 -0700

    Hides footer from printing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Index(props) {
     return (
-        <main className="col-lg-8 mx-auto p-3 py-md-5">
+        <main className="footer col-lg-8 mx-auto p-3 py-md-5">
             <footer className="pt-5 my-5 text-muted border-top">
                 Created by Daniel Vasquez 2022
             </footer>
