dd96391832102b5f3d212d86bb29bfe48063d4b1
Daniel Vasquez <main@dvasquez4155.com>
2025-08-24 16:34:10 -0700

    Added credits to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Index(props) {
     return (
         <main className="footer col-lg-8 mx-auto p-3 py-md-5">
             <footer className="pt-5 my-5 text-muted border-top">
-                Created by Daniel Vasquez 2022
+                Created by Daniel Vasquez 2022 | <Link to="/credits">Credits &amp; Sources</Link>
             </footer>
         </main>
     );
