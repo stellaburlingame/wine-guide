9052852934a0c52b8b20bfbd43173af7580d86b9
Daniel Vasquez <main@dvasquez4155.com>
2025-08-25 16:27:45 -0700

    Navebar now has active buttons

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 
 require('./index.css')
 function Index(props) {
     return (
         <main>
-        <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+        <Nav variant="underline" defaultActiveKey="/" className="navbar navbar-dark navbar-expand-lg bg-dark ">
             <div className="container">
-                <Link to="/" className="navbar-brand">
-                <Navbar.Brand href="#home">
+                <Navbar.Brand as={NavLink} to="/">
                     <img
                     alt=""
-                    src={`./assets/dark.svg`}
+                    src={`${process.env.PUBLIC_URL}/assets/dark.svg`}
                     width="30px"
                     height="30px"
                     className="d-inline-block align-top"
                     />{' '}
                     Stella Wine Guide
                 </Navbar.Brand>
-                </Link>
                 <div id="navbarNav">
                 <ul id="links" className="nav ml-auto">
-                    <Link variant="outline-*" className="nav-link" to="/">Home</Link>
-                    <Link variant="outline-*" className="nav-link" to="definitions">Definitions</Link>
-                    <Link variant="outline-*" className="nav-link" to="wine">Wines</Link>
-                    <Link variant="outline-*" className="nav-link" to="wine-top-picks">Top Wine Picks</Link>
+                    {/* <NavLink to="/" end className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
+                        Home
+                    </NavLink> */}
+                    <NavLink to="/definitions" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
+                        Definitions
+                    </NavLink>
+                    <NavLink to="/wine" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
+                        Wines
+                    </NavLink>
+                    <NavLink to="/wine-top-picks" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
+                        Top Wine Picks
+                    </NavLink>
                 </ul>
                 </div>
             </div>
-            </nav>
+            </Nav>
         </main>
     );
 }
