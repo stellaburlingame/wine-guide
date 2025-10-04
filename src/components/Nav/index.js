37b836b3dd252de01aa06155ee17e3ffd7d765d6
Daniel Vasquez <main@dvasquez4155.com>
2025-10-04 16:03:43 -0700

    Small styling changes in nav page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,45 +1,54 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav';
 import { NavLink } from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-require('./index.css')
-function Index(props) {
+require("./index.css");
+
+function Index() {
     return (
         <main>
-        <Nav variant="underline" defaultActiveKey="/" className="navbar navbar-dark navbar-expand-lg bg-dark ">
-            <div className="container">
-                <Navbar.Brand as={NavLink} to="/">
-                    <img
-                    alt=""
-                    src={`${process.env.PUBLIC_URL}/assets/dark.svg`}
-                    width="30px"
-                    height="30px"
-                    className="d-inline-block align-top"
-                    />{' '}
-                    Stella Wine Guide
-                </Navbar.Brand>
-                <div id="navbarNav">
-                <ul id="links" className="nav ml-auto">
-                    {/* <NavLink to="/" end className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
-                        Home
-                    </NavLink> */}
-                    {/* <NavLink to="/definitions" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
-                        Definitions
-                    </NavLink> */}
-                    <NavLink to="/wine-top-picks" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
-                        Top Wine Picks
-                    </NavLink>
-                    <NavLink to="/wine" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}>
-                        Wine List
-                    </NavLink>
-                    {/* <NavLink to="/find-your-wine" className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "") }>
-                        Find Your Wine
-                    </NavLink> */}
-                </ul>
-                </div>
-            </div>
-            </Nav>
+        {/* Navbar controls its own collapse. Make sure Bootstrap CSS is loaded in your app entry (e.g., index.js). */}
+        <Navbar expand="lg" bg="dark" variant="dark" className="justify-content-between">
+            <Container>
+            <Navbar.Brand as={NavLink} to="/">
+                <img
+                alt=""
+                src={`${process.env.PUBLIC_URL}/assets/dark.svg`}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                />{" "}
+                Stella Wine Guide
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav className="nav-links">
+                <NavLink
+                    to="/wine-top-picks"
+                    className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}
+                >
+                    Top Wine Picks
+                </NavLink>
+                <NavLink
+                    to="/wine"
+                    className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}
+                >
+                    Wine List
+                </NavLink>
+                {/*
+                <NavLink
+                    to="/find-your-wine"
+                    className={({ isActive }) => "nav-link" + (isActive ? " nav-active" : "")}
+                >
+                    Find Your Wine
+                </NavLink>
+                */}
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
         </main>
     );
 }
