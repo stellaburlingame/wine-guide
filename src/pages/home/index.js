3e85b0e42fc9625ba4543a5123644bacca48bcdb
Daniel Vasquez <main@dvasquez4155.com>
2025-11-03 10:23:45 -0800

    New wines introduced in seperate homepage button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -21,6 +21,11 @@ class index extends React.Component {
                             Explore our curated wine list, tasting notes, and recommended pairings
                         </h3>
                         <div className="d-flex justify-content-center gap-3 mt-4">
+                            <Link to="new-wines">
+                                <Button style={{backgroundColor: '#'}} variant="dark" size="lg">✨ Visit New Wines{' '}<FaArrowRight size={'2em'} /></Button>
+                            </Link>
+                        </div>
+                        <div className="d-flex justify-content-center gap-3 mt-4">
                             <Link to="wine">
                                 <Button variant="dark" size="lg">Go to Wine List{' '}<FaArrowRight size={'2em'} /></Button>
                             </Link>
